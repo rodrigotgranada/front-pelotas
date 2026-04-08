28772be96231c68a24780d1d450c5dedb86a0668
Rodrigo Granada <rtgranada@gmail.com>
2026-04-08 09:43:53 -0300

    chore: setup vercel dev environment variables and angular build configurations

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://back-pelotas-dev.onrender.com/api',
+  apiUrl: 'https://back-pelotas-dev.onrender.com/api',
+  publicBaseUrl: 'https://esporteclubepelotas-dev.vercel.app',
+};
--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // Esta URL deve ser substituída para a URL que o Render vai te fornecer após subir o backend
-  apiBaseUrl: 'https://back-pelotas.onrender.com/api',
-  apiUrl: 'https://back-pelotas.onrender.com/api',
-  publicBaseUrl: 'https://front-pelotas.vercel.app',
+  apiBaseUrl: 'https://back-pelotas-homolog.onrender.com/api',
+  apiUrl: 'https://back-pelotas-homolog.onrender.com/api',
+  publicBaseUrl: 'https://esporteclubepelotas.vercel.app',
 };
